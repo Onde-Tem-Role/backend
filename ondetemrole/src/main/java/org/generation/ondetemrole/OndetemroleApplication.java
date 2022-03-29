69451ebdfb28802955a6237889c74823e9cd26e3
unknown <jadson.vsantos2013@gmail.com>
2022-03-29 14:32:32 -0300

    Criando o Projeto Spring

---
+++ ondetemrole/src/main/java/org/generation/ondetemrole/OndetemroleApplication.java
@@ -0,0 +1,13 @@
+package org.generation.ondetemrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndetemroleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndetemroleApplication.class, args);
+	}
+
+}
---
+++ ondetemrole/src/test/java/org/generation/ondetemrole/OndetemroleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.ondetemrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndetemroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
