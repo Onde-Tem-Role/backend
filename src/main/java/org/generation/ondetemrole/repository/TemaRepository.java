ecfc16e37bfddb7f1bcc656c34e5a5a7f64a088f
Anderson <and_mendescosta@hotmail.com>
2022-05-13 21:31:06 -0300

    Implementação do delete

--- src/main/java/org/generation/ondetemrole/repository/TemaRepository.java
+++ src/main/java/org/generation/ondetemrole/repository/TemaRepository.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import org.generation.ondetemrole.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 	
-	public List<Tema> findAllByTipoContainingIgnoreCase(String tipo);
-	
+	public List <Tema> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
 	
 }
